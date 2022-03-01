6776bb657e62a685ab358f8f57abf80678909609
venkat9666 <venkatmamillapalli273@gmail.com>
2022-03-01 16:59:24 +0530

    Adding class and methods

---
+++ PrjBanking/PrjBanking/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjBanking
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+
+        public void GetEmployee()
+        {
+
+        }
+        public void DisplayEmployee()
+        {
+
+
+        }
+
+    }
+}
