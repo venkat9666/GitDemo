de366bf2752a1b89da9ef204e286b020ea32b252
venkat9666 <venkatmamillapalli273@gmail.com>
2022-03-02 10:35:47 +0530

    added displayproduct metod

---
+++ Prjonlineshopping/Prjonlineshopping/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prjonlineshopping
+{
+    internal class Class1
+    {
+
+        void getproduct()
+        {
+            Console.WriteLine("bag,mobile");
+            Console.WriteLine("bike");
+        }
+
+        void Displayproduct()
+        {
+
+        }
+    }
+}
